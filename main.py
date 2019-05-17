d63f566515430f123b9a692cbe8810d198ab15c5
Jean-Claude <jeanggi90@gmail.com>
2019-05-17 20:03:02 +0200

    📦 NEW: Basic picture in picture functionality

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+import os
+import urllib
+from subprocess import Popen, PIPE
+import rumps
+
+rumps.debug_mode(True)
+
+class App(rumps.App):
+
+	def __init__(self):
+		super(App, self).__init__('Overlay')
+		self.menu = ['Open in VLC']
+
+	def getCurrentUrl(self):
+		script = '''
+		    tell application "Firefox" to activate
+			tell application "System Events"
+				keystroke "l" using {command down}
+				keystroke "c" using {command down}
+				do shell script "pbpaste"
+			end tell
+		'''
+
+		proc = Popen(['/usr/bin/osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+		stdout, stderr = proc.communicate(script)
+		return stdout
+
+	@rumps.clicked('Open in VLC')
+	def openOverlay(self, _):
+		os.system("/Applications/VLC.app/Contents/MacOS/VLC " + self.getCurrentUrl())
+
+if __name__ == '__main__':
+	App().run()
